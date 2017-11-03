1335ff22c3d9295c834ce8ba519eb11ea7db6320
niku <niku@niku.name>
2017-11-04 01:25:50 +0900

    Make CI enable

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :nigiwaiki, Nigiwaiki.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: System.get_env("TRAVIS_POSTGRESQL_PASSWORD") || "postgres",
   database: "nigiwaiki_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
