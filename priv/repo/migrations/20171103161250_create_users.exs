334c15947cc6c7a14134f0913c71da9289d686b9
niku <niku@niku.name>
2017-11-04 01:14:26 +0900

    Create User model
    Main perpose of this commit is making `priv/repo/migrations` directory to avoid error when `mix ecto.migrate` executes.
    So, It doesn't have any additional column.
    
    $ mix phx.gen.schema User users

---
+++ priv/repo/migrations/20171103161250_create_users.exs
@@ -0,0 +1,12 @@
+defmodule Nigiwaiki.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      timestamps()
+    end
+
+  end
+end
