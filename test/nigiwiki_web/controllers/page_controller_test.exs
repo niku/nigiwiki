a559f59cf9a51fdf76dc45175df1d9a937c59d18
niku <niku@niku.name>
2017-11-07 01:42:09 +0900

    Fix a failing test

--- test/nigiwiki_web/controllers/page_controller_test.exs
+++ test/nigiwiki_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule NigiwikiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "elm-app"
   end
 end
