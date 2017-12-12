df088e6e8393d455c93f206ccf4634d112fe4ac8
niku <niku@niku.name>
2017-12-13 00:18:24 +0900

    Generate channel NigiwikiWeb.RoomChannel
    $ mix phx.gen.channel Room

--- lib/nigiwiki_web/channels/room_channel.ex
+++ lib/nigiwiki_web/channels/room_channel.ex
@@ -1,12 +1,29 @@
 defmodule NigiwikiWeb.RoomChannel do
-  use Phoenix.Channel
+  use NigiwikiWeb, :channel
 
-  def join("room:lobby", _message, socket) do
-    {:ok, socket}
+  def join("room:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
-  def handle_in("new:msg", message, socket) do
-    broadcast! socket, "new:msg", message
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (room:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
     {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
   end
 end
