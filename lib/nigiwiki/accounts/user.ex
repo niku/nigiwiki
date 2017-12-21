c9a2c1bd760c7f4fbb35f0e8d3e0b1163bc1dc9a
niku <niku@niku.name>
2017-12-21 20:45:26 +0900

    Generate context Accounts
    $ mix phx.gen.context Accounts User users

---
+++ lib/nigiwiki/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule Nigiwiki.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Nigiwiki.Accounts.User
+
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
